4d9c67abcffc86ad1ba15e1074fd31dfc56069fd
Susana LaCoder Benavidez <sbenavid@stanford.edu>
2018-07-17 10:12:37 -0700

    createPost successfully posts the blog conent to the list of posts.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
 
-export const FETCH_POSTS = 'FETCH_POSTS';
+export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=UNIQUE1234';
 
+/*Need to make network requests inside action creator so we have to install
+axios to make the actual request and we have to install redux promise to
+handle the asynchronous nature of the request itself
+
+Before we make use of either of those modules inside of here let's
+make sure that we wire up redux promise
+as a middleware inside of our application (top level index start js file).
+ */
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
@@ -14,14 +23,13 @@ export function fetchPosts() {
   };
 }
 
-/*Need to make network requests inside action creator so we have to install
-axios to make the actual request and we have to install redux promise to
-handle the asynchronous nature of the request itself
+/*Need to make action creator for handling of onSubmit.
+  values contains the blog post */
+export function createPost(values) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
 
-
-Before we make use of either of those modules inside of here let's
-make sure that we wire up redux promise
-as a middleware inside of our application.
-
- top level index start js file.
- */
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
+}
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,95 @@
 import React, {Component} from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {createPost} from '../actions';
 
 class PostsNew extends Component {
-  render() {
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className=`form-group ${touched && error ? 'has-danger' : ''}`;
     return (
-      <div>
-        PostsNew!
+      <div className={className}>
+        <label>{field.label}</label>
+        <input className="form-control"
+          type="text"
+          {...field.input}
+        />
+        <div className="text-help">
+        {/* Display any errors from input validation after it has been touched and there is an error. */}
+        { touched ?  error : ''}
+        </div>
       </div>
+    );
+  }
+
+  /*
+  values object contains title, category, and content
+  Inside of onSubmit we really want to be calling an action creator and that action creator will be
+  responsible for posting the API or simply me posting the post into the API.
+  I think that will first start off by creating our action creator and put our API request inside of it.
+  After that we'll then figure out exactly how to hook it up to this on submit function.
+  */
+  onSubmit(values) {
+    //this === component
+    //wire up onSubmit to action creator createPost
+      this.props.createPost(values);
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel </Link>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+//values is an object that contains all values that user entered into form
+//console.log(values) -> { title: 'asf', catgeories: 'asdfee', content: 'Hi!'}
+function validate(values) {
+  const errors = {};
+
+  //Validate inputs from 'values'
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 3 characters!";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter a category!";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content!";
+  }
+
+  //if errors is empty, the form is fine to submit
+  //if errors has any properties, redux form assumes form is invalid
+  return errors;
+}
+
+export default reduxForm({
+  validate, //validate: validate,
+  form:'PostsNewForm'
+})(
+  connect(null, { createPost })(PostsNew)
+);
